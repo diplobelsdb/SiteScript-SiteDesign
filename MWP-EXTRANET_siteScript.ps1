d17ec6a4b219d77df28ab6985aabb2c3069f5528
sabine <62460085+diplobelsdb@users.noreply.github.com>
2021-04-26 09:26:46 +0200

    Extranet sites sitescript
    Adding sitescript for extranet sites

---
+++ MWP-EXTRANET_siteScript.ps1
@@ -0,0 +1,50 @@
+﻿# https://developer.microsoft.com/json-schemas/sp/site-design-script-actions.schema.json
+#https://laurakokkarinen.com/the-ultimate-guide-to-sharepoint-site-designs-and-site-scripts/#limiting-who-can-use-site-designs
+
+
+# Connect-SPOService -url "https://diplobelsdb-admin.sharepoint.com"
+
+# Get all SiteScripts
+get-spositescript
+
+
+$EXTRANET_site_script = @'
+{
+    "$schema": "schema.json",
+    "actions": [  
+       {
+        "verb": "removeNavLink",
+        "displayName": "Conversations",
+        "isWebRelative": true
+        } 
+        , {
+        "verb": "removeNavLink",
+        "displayName": "Notebook",
+        "isWebRelative": true
+        }     
+    ],
+    "bindata": { },
+    "version": 1
+}
+'@
+
+# add SiteScript
+Add-SPOSiteScript  -Title "EXTRANET MWP"  -Content $EXTRANET_site_script  -Description "Removing navigation link 'Conversations'"
+$guid = Get-SPOSiteScript | where {$_.Title -EQ "EXTRANET MWP"} | Select -First 1 Id
+$guid.Id.ToString()  
+
+
+# update a previously uploaded Site Script
+$sitescriptID = Get-SPOSiteScript | where {$_.Title -EQ "EXTRANET MWP"} | Select -First 1 Id
+Set-SPOSiteScript -Identity $sitescriptID.Id  -Content $EXTRANET_site_script -Version 1
+
+
+# ---------
+# display sitescript content 
+$sitescriptID = Get-SPOSiteScript | where {$_.Title -EQ "EXTRANET MWP"} | Select -First 1 Id
+Get-SPOSiteScript $sitescriptID.Id 
+
+
+# remove scripts
+# Get-SPOSiteScript | where {$_.Title -EQ "EXTRANET MWP"} | ForEach-Object {Remove-SPOSiteScript -Identity $_.Id.ToString()}
+
--- MWP-siteDesign.ps1
+++ MWP-siteDesign.ps1
@@ -11,6 +11,20 @@ $guid_global = (Get-SPOSiteScript | where {$_.Title -EQ "Global MWP"} | Select -
 $guid_external = (Get-SPOSiteScript | where {$_.Title -EQ "External MWP"} | Select -First 1 Id).Id.Guid
 $guid_internal = (Get-SPOSiteScript | where {$_.Title -EQ "Internal MWP"} | Select -First 1 Id).Id.Guid
 $guid_posten = (Get-SPOSiteScript | where {$_.Title -EQ "Posten MWP"} | Select -First 1 Id).Id.Guid
+$guid_EXTRANET = (Get-SPOSiteScript | where {$_.Title -EQ "EXTRANET MWP"} | Select -First 1 Id).Id.Guid
+
+# export content sitescript to notepad
+$siteScriptTitle = 'TestListView'
+$id = get-spositescript | Where-Object Title -eq $siteScriptTitle | select Id
+get-spositescript -Identity $id.Id | select content | Out-File -FilePath "C:\data\SiteDesign\MWP-$($siteScriptTitle)_siteScript.ps1"
+
+# update existing sitescript - use PnP
+Connect-PnPOnline -Url https://diplobelsdb-admin.sharepoint.com/ -UseWebLogin
+
+$siteScriptTitle = 'TestListView'
+$id = Get-PnPSiteScript | Where-Object Title -eq $siteScriptTitle | select Id
+Set-PnPSiteScript -Identity $id.Id -Description 'Test adding listview'
+
 
 
 # add SiteDesign
@@ -27,7 +41,8 @@ Add-SPOSiteDesign -Title "MWP – external Posten site" -WebTemplate "64" -SiteS
 Add-SPOSiteDesign -Title "MWP – internal Communication site" -WebTemplate "68" -SiteScripts @($guid_internal, $guid_global)  -Description "Disabling external sharing, adding theme, adding custom columns for archiving, setting regional settings, customizing navigation" 
 Add-SPOSiteDesign -Title "MWP – external Communication site" -WebTemplate "68" -SiteScripts @($guid_external, $guid_global)  -Description "Enabling external sharing, adding theme, adding custom columns for archiving, setting regional settings, customizing navigation" 
 
-# Add-SPOSiteDesign -Title "MWP – internal Communication site" -WebTemplate "68" -SiteScripts $guid.Id.ToString()  -Description "Disabling external sharing, adding theme, adding custom columns for archiving, setting regional settings, customizing navigation" 
+# EXTRANET site - 64
+Add-SPOSiteDesign -Title "MWP – EXTRANET site" -WebTemplate "64" -SiteScripts @($guid_external, $guid_global, $guid_EXTRANET)  -Description "Enabling external sharing, adding theme, adding custom columns for archiving, setting regional settings, customizing navigation" 
 
 # ---------
 # display sitescript content 
